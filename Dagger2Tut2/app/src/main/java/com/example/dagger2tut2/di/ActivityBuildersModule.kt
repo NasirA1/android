eb60dba9cfff4d57c666642756ff043bf9561c6d
Nasir Ahmad <Nasir.Ahmad@optomany.com>
2020-10-25 20:49:48 +0000

    added

--- Dagger2Tut2/app/src/main/java/com/example/dagger2tut2/di/ActivityBuildersModule.kt
+++ Dagger2Tut2/app/src/main/java/com/example/dagger2tut2/di/ActivityBuildersModule.kt
@@ -3,7 +3,6 @@ package com.example.dagger2tut2.di
 import com.example.dagger2tut2.di.auth.AuthModule
 import com.example.dagger2tut2.di.auth.AuthViewModelsModule
 import com.example.dagger2tut2.ui.auth.AuthActivity
-import com.example.dagger2tut2.ui.auth.AuthViewModel
 import com.example.dagger2tut2.ui.main.MainActivity
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
